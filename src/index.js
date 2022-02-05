ed86401bec64394d2ff1c55c0aa96ea7bd9fc295
Adair <adair.reyes450@gmail.com>
2022-02-04 21:47:41 -0600

    Initial commit: The current version can handle adding new notes and also editing the existing ones, the design is still provisional and there is space for implementing more features

--- src/index.js
+++ src/index.js
@@ -1,17 +1,81 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Note(props) {
+  const [day, month, year] = [
+    props.date.getDate(),
+    props.date.getMonth(),
+    props.date.getFullYear(),
+  ];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  return (
+    <li className="note">
+      <div className="note-info">
+        <div className="note-title" contentEditable>{props.title}</div>
+        <div className="note-date" >
+          created on {day}/{month}/{year}
+        </div>
+      </div>
+      <div className="note-content" contentEditable>{props.content}</div>
+    </li>
+  );
+}
+
+class Notes extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: [
+        {
+          title: "My first note",
+          date: new Date(),
+          content: "This is my first note ever",
+        },
+      ],
+    };
+  }
+
+  handleButtonClick(e) {
+    const notes = [...this.state.notes];
+    const newNote = {
+      title: "Title",
+      date: new Date(),
+      content: "Enter text",
+    };
+    notes.push(newNote);
+    this.setState({
+      notes: notes,
+    });
+  }
+
+  render() {
+    const notesComponents = this.state.notes.map((note, index) => {
+      return (
+        <Note
+          title={note.title}
+          date={note.date}
+          content={note.content}
+          key={index}
+        />
+      );
+    });
+    return (
+      <ul className="notes-container">
+        {notesComponents}
+        <button
+          className="new-note-button"
+          onClick={(e) => this.handleButtonClick(e)}
+        >
+          New note
+        </button>
+      </ul>
+    );
+  }
+}
+
+function App() {
+  return <Notes />;
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
