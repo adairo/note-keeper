5d9baa9a053b757db136eec385a5fd31d6de9c1f
Adair <adair.reyes450@gmail.com>
2022-02-05 07:30:57 -0600

    add: now we have a better color palette and there is a three-dot-button on the right corner of every note, the delete button is not working properly, it deletes always the last note regardless of which note was selected

--- src/index.js
+++ src/index.js
@@ -2,23 +2,87 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-function Note(props) {
-  const [day, month, year] = [
-    props.date.getDate(),
-    props.date.getMonth(),
-    props.date.getFullYear(),
-  ];
-
+function PopUpMenu(props) {
   return (
-    <li className="note">
-      <div className="note-info">
-        <div className="note-title" contentEditable>{props.title}</div>
-        <div className="note-date" >
-          created on {day}/{month}/{year}
+    <div
+      className={`pop-up-menu ${props.active ? "active" : ""}`}
+      tabIndex={"0"}
+    >
+      <option className="pop-up-option" onClick={props.handleDelete}>
+        Delete
+      </option>
+      <option className="pop-up-option">Edit</option>
+    </div>
+  );
+}
+
+class Note extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "New title",
+      date: new Date(),
+      content: "Enter your text here",
+      showingPopUp: false,
+    };
+  }
+
+  handleToggleMenu() {
+    this.setState({
+      showingPopUp: !this.state.showingPopUp,
+    });
+  }
+
+  handleTitleInput(e) {
+    const newTitle = e.target.textContent.trim();
+    console.log(newTitle)
+
+    this.setState({
+      title: newTitle
+    })
+  }
+
+  render() {
+    const [day, month, year] = [
+      this.props.date.getDate(),
+      this.props.date.getMonth(),
+      this.props.date.getFullYear(),
+    ];
+
+    return (
+      <li className="note">
+        <div className="note-info">
+
+        <ThreeDotButton onClick={this.handleToggleMenu.bind(this)}/>
+
+          <PopUpMenu
+            active={this.state.showingPopUp}
+            handleDelete={() => this.props.handleDelete(this.props.noteId)}>
+          </PopUpMenu>
+
+          <div 
+            className="note-title" 
+            contentEditable={true}
+            onInput={(e) => {this.handleTitleInput(e)}}
+          >
+            {this.state.title}</div>
+          <div className="note-date">
+            created on {day}/{month}/{year}
+          </div>
         </div>
-      </div>
-      <div className="note-content" contentEditable>{props.content}</div>
-    </li>
+        <div className="note-content" contentEditable={true}>{this.state.content}</div>
+      </li>
+    );
+  }
+}
+
+function ThreeDotButton(props) {
+  return (
+    <div className="three-dot-button" onClick={props.onClick}>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+    </div>
   );
 }
 
@@ -26,14 +90,19 @@ class Notes extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      notes: [
-        {
-          title: "My first note",
-          date: new Date(),
-          content: "This is my first note ever",
-        },
-      ],
+      notes: [],
     };
+  }
+
+  handleDeleteNote(noteId) {
+    const notes = [...this.state.notes];
+    // const noteId = this.props.noteId;
+
+    notes.splice(noteId, 1);
+    console.log(notes)
+    this.setState({
+      notes: notes,
+    });
   }
 
   handleButtonClick(e) {
@@ -57,6 +126,8 @@ class Notes extends React.Component {
           date={note.date}
           content={note.content}
           key={index}
+          noteId={index}
+          handleDelete={this.handleDeleteNote.bind(this)}
         />
       );
     });
